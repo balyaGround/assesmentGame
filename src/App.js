4ae1b4b02443d5098748bb04c1655a0f53cb0597
balyaGround <balyazulfikar@gmail.com>
2021-10-03 20:06:24 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Haloooo Adek Sayang bisa dunks
+          Haloooo Hilda Chocochips Adek Sayang bisa dunks
         </a>
       </header>
     </div>
